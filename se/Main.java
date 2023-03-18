b09ed0a122eeff89de1d95b496d10d70b39a36a8
shinbi <jennykang97@naver.com>
2023-03-19 01:35:12 +0900

    TEST

--- se/Main.java
+++ se/Main.java
@@ -1,14 +1,10 @@
 package se;
 
-//1:remote by jennykang97
-//2:local by jennykang97
-//2: remote by jennykang97
-//1: localbranch01 by jennykang97 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test");
+		System.out.println("localbranch_Test");
 	}
 
 }

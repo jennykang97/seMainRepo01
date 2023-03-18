a77ff411660d72943c7ee3dcb437777e8ca74a3c
shinbi <jennykang97@naver.com>
2023-03-19 01:52:42 +0900

    TEST0152

--- se/Main.java
+++ se/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test_branch01");
+		System.out.println("localbranch_Test");
 	}
 
 }

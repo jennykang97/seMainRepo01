08cda93b970588cb8a71b2f6a628e4da52cf44e8
USER <USER@AL02355167>
2023-03-19 00:18:55 +0900

    commitTest_master

--- se/Main.java
+++ se/Main.java
@@ -3,11 +3,12 @@ package se;
 //1:remote by jennykang97
 //2:local by jennykang97
 //2: remote by jennykang97
+//1: localbranch01 by jennykang97 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		System.out.println("Helloworld_commitTest");
 	}
 
 }
